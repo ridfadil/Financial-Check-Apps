d7114ecf1bad076b0aa146805ab0afbccd4d33ba
ridfadil <bbmfrd@gmail.com>
2018-09-22 22:14:27 +0700

    update

--- app/src/main/java/org/properti/analisa/financialcheck/activity/UserProfilActivity.java
+++ app/src/main/java/org/properti/analisa/financialcheck/activity/UserProfilActivity.java
@@ -9,11 +9,23 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.properti.analisa.financialcheck.R;
+import org.properti.analisa.financialcheck.model.Common;
+import org.properti.analisa.financialcheck.model.User;
+
+import java.util.LinkedList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static org.properti.analisa.financialcheck.activity.MainActivity.UID_USER;
 
 public class UserProfilActivity extends AppCompatActivity {
 
@@ -27,13 +39,48 @@ public class UserProfilActivity extends AppCompatActivity {
     TextView tvProfilEmail;
     @BindView(R.id.btn_edit_profil)
     Button btnEditProfil;
+    DatabaseReference dbUser;
+    String idUser;
+    private final LinkedList<User> listMenu = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profil);
         ButterKnife.bind(this);
-        toolbar();
+        //toolbar();
+        idUser = getIntent().getStringExtra(UID_USER);
+        dbUser = FirebaseDatabase.getInstance().getReference("users").child(idUser);
+
+        String nama = (listMenu.get(0).getNama());
+        String noTelp = (listMenu.get(0).getPhone());
+        String email = (listMenu.get(0).getEmail());
+
+        tvProfilNamaUser.setText(nama);
+        tvProfilNoHp.setText(noTelp);
+        tvProfilEmail.setText(email);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        dbUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+               listMenu.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    User userProfile = postSnapshot.getValue(User.class);
+                    listMenu.add(userProfile);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @OnClick(R.id.btn_edit_profil)
